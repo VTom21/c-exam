7e4bf250b4a2a0e5d15fb17e7f71e7599a95dee3
Ny20VisegrádiT <Ny20VisegrdiT@203-12>
2025-01-22 10:57:18 +0100

    Add project files.

---
+++ yes/Program.cs
@@ -0,0 +1,57 @@
+﻿using yes;
+
+List<Sebessegkategoria> items = new List<Sebessegkategoria>();
+
+List<int> utas_1 = new List<int>();
+List<int> utas_2 = new List<int>();
+
+
+using (StreamReader reader = new StreamReader("C:\\Users\\Ny20VisegrádiT\\source\\repos\\yes\\yes\\src\\utasszallitok.txt"))
+{
+    while (!reader.EndOfStream)
+    {
+        string[] array = reader.ReadLine().Split(';');
+
+        if (array.Length >= 7)
+        {
+            items.Add(new Sebessegkategoria(array[0], double.Parse(array[1]),array[2], array[3], double.Parse(array[4]), double.Parse(array[5]), double.Parse(array[6])));
+        }
+    }
+}
+Console.WriteLine("3.feladat: \n");
+foreach (var item in items)
+{
+    Console.WriteLine($"{item.tipus}");
+}
+
+
+Console.WriteLine("4.feladat: \n");
+
+Console.WriteLine($"Adatsorok száma: {items.Count()}");
+
+Console.WriteLine("5.feladat: \n");
+
+Console.WriteLine($"A Boeing típusok száma: {items.Where(n => n.tipus.Contains("Boeing")).Count()}");
+
+Console.WriteLine("7.feladat: \n");
+
+foreach (var item in items)
+{
+    if (item.utas.Contains('-'))
+    {
+        utas_1.Add(int.Parse(item.utas.Split('-')[0]));
+        utas_1.Add(int.Parse(item.utas.Split('-')[0]));
+    }
+    else
+    {
+        utas_1.Add(int.Parse(item.utas));
+    }
+}
+
+
+
+
+
+
+
+
---
+++ yes/szallitok.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace yes
+{
+
+    class Sebessegkategoria
+    {
+        public string tipus;
+        public double ev;
+        public string utas;
+        public string szemelyzet;
+        public double felszallo_tomeg;
+        public double fesztav;
+        public double Utazosebesseg;
+
+        public Sebessegkategoria(string tipus, double ev, string utas, string szemelyzet, double felszallo_tomeg, double fesztav, double utazosebesseg)
+        {
+            this.tipus = tipus;
+            this.ev = ev;
+            this.utas = utas;
+            this.szemelyzet = szemelyzet;
+            this.felszallo_tomeg = felszallo_tomeg;
+            this.fesztav = fesztav;
+            Utazosebesseg = utazosebesseg;
+        }
+
+        public string Kategorianev
+        {
+            get
+            {
+                if (Utazosebesseg < 500) return "Alacsony sebességű";
+                else if (Utazosebesseg < 1000) return "Szubszonikus";
+                else if (Utazosebesseg < 1200) return "Transzszonikus";
+                else return "Szuperszonikus";
+            }
+        }
+    }
+
+    internal class szallitok
+    {
+
+    }
+}
